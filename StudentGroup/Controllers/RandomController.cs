2896ea4218c3493675de54cc7dbf3c79a95f4c52
Sayavush01 <gurbanovseyavush@gmail.com>
2026-04-26 14:31:12 +0400

    commits

---
+++ StudentGroup/Controllers/RandomController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentGroup.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RandomController: Controller
+    {
+        [HttpGet]
+        public string GetRandomStudent()
+        {
+            var students = new List<string> { "Alice", "Bob", "Charlie", "David", "Eve" };
+            var random = new Random();
+            int index = random.Next(students.Count);
+            return students[index];
+        }
+    }
+}
