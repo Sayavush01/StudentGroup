a96a9d98e30e97cfc471659194cd5c30821f864d
Sayavush01 <gurbanovseyavush@gmail.com>
2026-05-07 23:23:19 +0400

    new commits

---
+++ StudentGroup/Helpers/FIleHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace StudentGroup.Helpers;
+
+public static class FileHelper
+{
+    public static async Task<string> SaveFileAsync(IFormFile file, string folderName)
+    {
+        var uploadsFolder = Path.Combine("wwwroot", "uploads", folderName);
+
+        if (!Directory.Exists(uploadsFolder))
+            Directory.CreateDirectory(uploadsFolder);
+
+        var fileName = $"{Guid.NewGuid()}_{file.FileName}";
+        var filePath = Path.Combine(uploadsFolder, fileName);
+
+        using var stream = new FileStream(filePath, FileMode.Create);
+        await file.CopyToAsync(stream);
+
+        return $"/uploads/{folderName}/{fileName}";
+    }
+}
